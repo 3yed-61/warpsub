da23d5accc57c23d2f45a4e3fceef7156de10ac1
3λΞĐ <122279300+3yed-61@users.noreply.github.com>
2024-06-19 14:51:56 +0330

    Update crawler.php

--- crawler.php
+++ crawler.php
@@ -8,7 +8,7 @@
     $warp .= "//subscription-userinfo: upload=0; download=0; total=10737418240000000; expire=0\n";
     $warp .= "//support-url: https://github.com/3yed-61\n";
     $warp .= "//profile-web-page-url: https://github.com/3yed-61\n\n";
-    $warp .= "warp://auto#🇮🇷 WARP &&detour=warp://auto#🇩🇪 WARP";
+   
 
    $i = 1;
 $pattern = '/^warp:\/\/.*$/';
